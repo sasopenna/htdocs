094059f42270ccbdcffba50a540dd8e35291d662
sasopenna <salvopenna97@gmail.com>
2018-02-28 21:25:14 +0100

    rimosso il keydown e messo nel grid.js

--- 2048/index.js
+++ 2048/index.js
@@ -6,9 +6,3 @@ for (let x = 0; x < 8; x++) {
   addNumber(agrid);
 }
 updateHTMLgrid("table_grid", agrid);
-
-document.addEventListener('keydown', (event) => {
-  const k = event.keyCode - 37;
-  if (k < 0 || k > 3) return;
-  combineNumbers(k, agrid);
-});

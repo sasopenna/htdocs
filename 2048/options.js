8db9c378c4d141010da48fe965e29f1eed242fd4
sasopenna <salvopenna97@gmail.com>
2018-02-28 21:25:29 +0100

    file opzioni

---
+++ 2048/options.js
@@ -0,0 +1,50 @@
+let cellOptions = {
+  "0": {
+    size: 28,
+    color: "#CCC"
+  },
+  "2": {
+    size: 28,
+    color: "#e5ffe5"
+  },
+  "4": {
+    size: 28,
+    color: "#ccffcc"
+  },
+  "8": {
+    size: 28,
+    color: "#e59400"
+  },
+  "16": {
+    size: 28,
+    color: "#cc8400"
+  },
+  "32": {
+    size: 28,
+    color: "#b27300"
+  },
+  "64": {
+    size: 28,
+    color: "#b27300"
+  },
+  "128": {
+    size: 28,
+    color: "#ffff99"
+  },
+  "256": {
+    size: 28,
+    color: "#ffff99"
+  },
+  "512": {
+    size: 28,
+    color: "#ffff99"
+  },
+  "1024": {
+    size: 28,
+    color: "#ffff99"
+  },
+  "2048": {
+    size: 28,
+    color: "#ffff99"
+  }
+}

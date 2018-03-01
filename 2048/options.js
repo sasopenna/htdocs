c93c215b63b4347143ead95f99d11406c5ad7b7c
sasopenna <salvopenna97@gmail.com>
2018-03-01 13:36:52 +0100

    changes

--- 2048/options.js
+++ 2048/options.js
@@ -1,50 +1,67 @@
 let cellOptions = {
   "0": {
     size: 28,
-    color: "#CCC"
+    color: "#000",
+    bgcolor: "#CCC"
   },
   "2": {
     size: 28,
-    color: "#e5ffe5"
+    color: "#000",
+    bgcolor: "#eee4da"
   },
   "4": {
     size: 28,
-    color: "#ccffcc"
+    color: "#000",
+    bgcolor: "#ede0c8"
   },
   "8": {
     size: 28,
-    color: "#e59400"
+    color: "#f9f6f2",
+    bgcolor: "#f2b179"
   },
   "16": {
     size: 28,
-    color: "#cc8400"
+    color: "#f9f6f2",
+    bgcolor: "#f59563"
   },
   "32": {
     size: 28,
-    color: "#b27300"
+    color: "#f9f6f2",
+    bgcolor: "#f67c5f"
   },
   "64": {
     size: 28,
-    color: "#b27300"
+    color: "#f9f6f2",
+    bgcolor: "#f65e3b"
   },
   "128": {
     size: 28,
-    color: "#ffff99"
+    color: "#f9f6f2",
+    bgcolor: "#edcf72"
   },
   "256": {
     size: 28,
-    color: "#ffff99"
+    color: "#f9f6f2",
+    bgcolor: "#edcc61"
   },
   "512": {
     size: 28,
-    color: "#ffff99"
+    color: "#f9f6f2",
+    bgcolor: "#edc850"
   },
   "1024": {
-    size: 28,
-    color: "#ffff99"
+    size: 16,
+    color: "#f9f6f2",
+    bgcolor: "#edc53f"
   },
   "2048": {
-    size: 28,
-    color: "#ffff99"
+    size: 16,
+    color: "#f9f6f2",
+    bgcolor: "#edc22e"
+  },
+  "-1": {
+    size: 16,
+    color: "#f9f6f2",
+    bgcolor: "3c3a32"
   }
 }
